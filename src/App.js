1dd831d6063649f143ceb0feb216fd619ac1b1c8
mwilliams659 <63402316+mwilliams659@users.noreply.github.com>
2020-09-21 13:47:01 +0100

    Robot Friends

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './Robots';
+
+// STATE = the description of your app. An object that describes app.
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+    render() {
+        
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })       
+        return (
+            <div className='tc'>
+                <h1>RobotFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots = {filteredRobots}/>
+            </div>
+        );
+    } 
+}
+
+export default App;
